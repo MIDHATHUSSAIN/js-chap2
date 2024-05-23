db78a97d5832f401fff00b1bb451d2f0af993ae3
MIDHAT-HUSSAIN <135623033+MIDHATHUSSAIN@users.noreply.github.com>
2024-05-23 13:53:57 +0500

    Add files via upload

---
+++ variablesForString.js
@@ -0,0 +1,56 @@
+// Question no:1
+
+let userName; 
+
+// Question no:2
+
+let myName="alex"
+
+
+// Question no:3
+
+let message="Hello world";
+
+alert(message);
+
+// Question no:4
+
+let studentName= "Jhone Doe";
+
+let age="15 years old";
+
+let education="Certified Mobile Application Development";
+
+alert(studentName);
+alert(age);
+alert(education);
+
+// Question no:5
+
+let pizzaa = "pizza \n pizz \n piz \n pi \n p";
+
+alert(pizzaa);
+
+// Question no:6
+
+let email="hafsaahusssain@gmail.com"
+
+alert("My email address is " + email);
+
+// Question no:7
+
+let Book="A smarter \n way to learn JavaScript";
+
+alert("I am trying to learn from the Book " + Book);
+
+// Question no:8
+
+document.write("Yah! I can write HTML content through JavaScript. <br> <br> ");
+
+// Question no:9
+
+let style=" ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+
+alert(style);
+
+document.write(" ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ");
